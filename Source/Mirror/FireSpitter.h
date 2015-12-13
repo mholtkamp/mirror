8e60ed012a2942a77a4c3601dc32e27bf41cca16
dslamontagne <3dslamontagne@gmail.com>
2015-12-12 21:59:03 -0800

    ui of cloud and start of firespitter

---
+++ Source/Mirror/FireSpitter.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "FireSpitter.generated.h"
+
+UCLASS()
+class MIRROR_API AFireSpitter : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFireSpitter();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+    UPROPERTY(EditAnywhere)
+    UStaticMeshComponent* m_pMesh;
+
+    UPROPERTY(EditAnywhere)
+    float m_fCountdown;
+	
+};
